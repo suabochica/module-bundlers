6ee0d673668ff711594a0a77c98f937b5c0d2287
Sergio L. Benítez D <sergio.kun21@gmail.com>
2021-04-27 08:46:12 -0500

    module-bundlers: rollup setup

---
+++ rollup/rollup.config.js
@@ -0,0 +1,26 @@
+import babel from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
+import postcss from "rollup-plugin-postcss";
+import replace from "@rollup/plugin-replace";
+import resolve from "@rollup/plugin-node-resolve";
+
+import * as path from "path";
+
+export default {
+  input: "src/index.js",
+  output: {
+    file: path.resolve(__dirname, path.join("public", "bundle.js")),
+  },
+  plugins: [
+    babel({
+      babelHelpers: "bundled",
+      exclude: "node_modules/**",
+    }),
+    commonjs(),
+    postcss(),
+    replace({
+      "process.env.NODE_ENV": JSON.stringify("production"),
+    }),
+    resolve(),
+  ],
+};
